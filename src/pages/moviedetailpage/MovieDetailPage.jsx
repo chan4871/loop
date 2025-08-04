12dff6acb8b7fe1a574a00b483e3e5e96a59a447
chan4871 <chan4871@gmail.com>
2025-08-04 19:32:07 +0900

    Update MovieDetailPage.jsx

--- src/pages/moviedetailpage/MovieDetailPage.jsx
+++ src/pages/moviedetailpage/MovieDetailPage.jsx
@@ -111,7 +111,7 @@ if (!data) {
         </Modal.Header>
         <ModalBody>
         {trailer? <div className="ratio ratio-16x9">
-            <iframe src={`https://www.youtube.com/embed/${trailer.key}` title='영화 트레일러'}></iframe>
+            <iframe src={`https://www.youtube.com/embed/${trailer.key}`} title='영화 트레일러'></iframe>
           </div> : <p>예고편 없음</p>
           }
         </ModalBody>
@@ -163,5 +163,6 @@ if (!data) {
 
 
 export default MovieDetailPage
+
 
 
